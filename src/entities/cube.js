1f86b0fae46960dfc49883bbaa07eafc1793a423
codeitdown <sumitbopche01@gmail.com>
2020-07-10 10:20:20 +0530

    removed comments

--- src/entities/cube.js
+++ src/entities/cube.js
@@ -18,6 +18,9 @@ export default class Cube {
             onComplete: this.gotHit,
             onCompleteParams: ["Got hit by the cube"],
         });
+        this.points_array = []
+        this.cube_color = "rgba(10, 250, 0, 0.3)";
+
     }
     // Do some math to project the 3D position into the 2D canvas
     project(x, y, z) {
@@ -52,8 +55,6 @@ export default class Cube {
         // if (this.z < -FIELD_OF_VIEW + this.radius) {
         //   return;
         // }
-        this.points_array = []
-        this.cube_color = "rgba(10, 250, 0, 0.3)";
         for (let i = 0; i < CUBE_LINES.length; i++) {
 
             const v1 = {
@@ -68,17 +69,13 @@ export default class Cube {
             };
             const v1Project = this.project(v1.x, v1.y, v1.z);
             const v2Project = this.project(v2.x, v2.y, v2.z);
-            // console.log([v1Project.x, v1Project.y, v2Project.x, v2Project.y])
+
             this.points_array.push([v1Project.x, v1Project.y, v2Project.x, v2Project.y])
             if (this.points_array.length == 12) {
                 console.log("point array size 12")
                 this.drawCube()
                 this.points_array = []
             }
-            // this.ctx.beginPath();
-            // this.ctx.moveTo(v1Project.x, v1Project.y);
-            // this.ctx.lineTo(v2Project.x, v2Project.y);
-            // this.ctx.stroke();
         }
     }
 
@@ -91,24 +88,6 @@ export default class Cube {
         this.drawBackFace();
     }
 
-    //Left face
-    // line 3 6 9 4
-
-    // [
-    //     [0,1,2,3],// 0
-    //     [0,1,2,3],// 1
-    //     [0,1,2,3],// 2
-    //     [0,1,2,3],// 3 0 1 moveTo step 1 | 2 3 lineTo step 2
-    //     [0,1,2,3],// 4 0 1 lineTo step 5
-    //     [0,1,2,3],// 5
-    //     [0,1,2,3],// 6 2 3 lineTo step 3
-    //     [0,1,2,3],// 7 
-    //     [0,1,2,3],// 8
-    //     [0,1,2,3],// 9 0 1 lineTo step 4
-    //     [0,1,2,3],// 10
-    //     [0,1,2,3],// 11
-    // ]
-    //
     drawLeftFace() {
         this.ctx.beginPath();
         this.ctx.moveTo(this.points_array[3][0], this.points_array[3][1]);
@@ -122,24 +101,6 @@ export default class Cube {
         this.ctx.stroke();
     }
 
-    //upper face
-    // line 0 7 11 6
-
-    // [
-    //     [0,1,2,3],// 0 0 1 moveTo step 1 | 2 3 lineTo step 2
-    //     [0,1,2,3],// 1 
-    //     [0,1,2,3],// 2
-    //     [0,1,2,3],// 3 
-    //     [0,1,2,3],// 4 
-    //     [0,1,2,3],// 5
-    //     [0,1,2,3],// 6 0 1 lineTo step 5 
-    //     [0,1,2,3],// 7 2 3 lineTo step 3
-    //     [0,1,2,3],// 8
-    //     [0,1,2,3],// 9 
-    //     [0,1,2,3],// 10
-    //     [0,1,2,3],// 11 0 1 lineTo step 4
-    // ]
-    //
     drawUpperFace() {
         this.ctx.beginPath();
         this.ctx.moveTo(this.points_array[0][0], this.points_array[0][1]);
@@ -152,25 +113,6 @@ export default class Cube {
         this.ctx.fill()
         this.ctx.stroke();
     }
-
-    //right face
-    // line 1 5 10 7
-
-    // [
-    //     [0,1,2,3],// 0 0 1 moveTo step 1 | 2 3 lineTo step 2
-    //     [0,1,2,3],// 1 
-    //     [0,1,2,3],// 2
-    //     [0,1,2,3],// 3 
-    //     [0,1,2,3],// 4 
-    //     [0,1,2,3],// 5 2 3 lineTo step 3
-    //     [0,1,2,3],// 6  
-    //     [0,1,2,3],// 7 0 1 lineTo step 5
-    //     [0,1,2,3],// 8
-    //     [0,1,2,3],// 9 
-    //     [0,1,2,3],// 10 2 3 lineTo step 4
-    //     [0,1,2,3],// 11 
-    // ]
-    //
 
     drawRightFace() {
         this.ctx.beginPath();
@@ -185,25 +127,6 @@ export default class Cube {
         this.ctx.stroke();
     }
 
-    //Bottom face
-    // line 2 4 8 5
-
-    // [
-    //     [0,1,2,3],// 0 
-    //     [0,1,2,3],// 1 
-    //     [0,1,2,3],// 2 0 1 moveTo step 1 | 2 3 lineTo step 2
-    //     [0,1,2,3],// 3 
-    //     [0,1,2,3],// 4 2 3 lineTo step 3
-    //     [0,1,2,3],// 5 0 1 lineTo step 5
-    //     [0,1,2,3],// 6  
-    //     [0,1,2,3],// 7 
-    //     [0,1,2,3],// 8 2 3 lineTo step 4
-    //     [0,1,2,3],// 9 
-    //     [0,1,2,3],// 10 
-    //     [0,1,2,3],// 11 
-    // ]
-    //
-
     drawBottomFace() {
         this.ctx.beginPath();
         this.ctx.moveTo(this.points_array[2][0], this.points_array[2][1]);
@@ -217,25 +140,6 @@ export default class Cube {
         this.ctx.stroke();
     }
 
-    //Front face
-    // line 0 1 2 3
-
-    // [
-    //     [0,1,2,3],// 0 0 1 moveTo step 1 | 2 3 lineTo step 2
-    //     [0,1,2,3],// 1 2 3 lineTo step 3
-    //     [0,1,2,3],// 2 2 3 lineTo step 4
-    //     [0,1,2,3],// 3 2 3 lineTo step 5
-    //     [0,1,2,3],// 4 
-    //     [0,1,2,3],// 5 
-    //     [0,1,2,3],// 6  
-    //     [0,1,2,3],// 7 
-    //     [0,1,2,3],// 8 
-    //     [0,1,2,3],// 9 
-    //     [0,1,2,3],// 10 
-    //     [0,1,2,3],// 11 
-    // ]
-    //
-
     drawFrontFace() {
         this.ctx.beginPath();
         this.ctx.moveTo(this.points_array[0][0], this.points_array[0][1]);
@@ -248,24 +152,6 @@ export default class Cube {
         this.ctx.fill()
         this.ctx.stroke();
     }
-
-    //Back face
-    // line 8 10 11 9
-    // [
-    //     [0,1,2,3],// 0 
-    //     [0,1,2,3],// 1 
-    //     [0,1,2,3],// 2 
-    //     [0,1,2,3],// 3 
-    //     [0,1,2,3],// 4 
-    //     [0,1,2,3],// 5 
-    //     [0,1,2,3],// 6  
-    //     [0,1,2,3],// 7 
-    //     [0,1,2,3],// 8 0 1 moveTo step 1 | 2 3 lineTo step 2
-    //     [0,1,2,3],// 9 0 1 lineTo step 5
-    //     [0,1,2,3],// 10 2 3 lineTo step 3
-    //     [0,1,2,3],// 11 0 1 lineTo step 4
-    // ]
-    //
 
     drawBackFace(){
         this.ctx.beginPath();
