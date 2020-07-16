37c507d1fd1e0922e41d353dc6bf11f08e0b6ec0
codeitdown <sumitbopche01@gmail.com>
2020-07-17 02:29:31 +0530

    cubes of 20 pixel to improve performance than 10

--- src/usecases/createShatteredCubeEntities.js
+++ src/usecases/createShatteredCubeEntities.js
@@ -10,8 +10,8 @@ export default function createShatteredCubeEntities(entity) {
     let x_min = entity.last_points[0][0], y_min = entity.last_points[0][1],
         x_max = entity.last_points[1][2], y_max = entity.last_points[1][3];
     //create small cubes
-    let rows = Math.floor((x_max - x_min) / 10);
-    let columns = Math.floor((y_max - y_min) / 10);
+    let rows = Math.floor((x_max - x_min) / 20);
+    let columns = Math.floor((y_max - y_min) / 20);
     let current_x = x_min;
     let current_y = y_min;
     // console.log('rows and columns ', rows, columns);
@@ -25,9 +25,9 @@ export default function createShatteredCubeEntities(entity) {
                 gameWidth: window.gameWidth
             }
             shattered_cube_list.push(new ShatteredCube(data, entity));
-            current_x += 10
+            current_x += 20
         }
-        current_y += 10;
+        current_y += 20;
         current_x = x_min
     }
     return shattered_cube_list;
