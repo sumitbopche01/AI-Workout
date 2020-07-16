7f25dd47ddf1cbd66b5f8108709d87175d2ea444
codeitdown <sumitbopche01@gmail.com>
2020-07-16 19:55:27 +0530

    creating smaller cubes

--- src/usecases/createShatteredCubeEntities.js
+++ src/usecases/createShatteredCubeEntities.js
@@ -10,8 +10,8 @@ export default function createShatteredCubeEntities(entity) {
     let x_min = entity.last_points[0][0], y_min = entity.last_points[0][1],
         x_max = entity.last_points[1][2], y_max = entity.last_points[1][3];
     //create small cubes
-    let rows = Math.floor((x_max - x_min) / 50);
-    let columns = Math.floor((y_max - y_min) / 50);
+    let rows = Math.floor((x_max - x_min) / 10);
+    let columns = Math.floor((y_max - y_min) / 10);
     let current_x = x_min;
     let current_y = y_min;
     // console.log('rows and columns ', rows, columns);
@@ -24,10 +24,10 @@ export default function createShatteredCubeEntities(entity) {
                 gameHeight: window.gameHeight,
                 gameWidth: window.gameWidth
             }
-            shattered_cube_list.push(new ShatteredCube(data, entity.ctx));
-            current_x += 50
+            shattered_cube_list.push(new ShatteredCube(data, entity));
+            current_x += 10
         }
-        current_y += 50;
+        current_y += 10;
         current_x = x_min
     }
     return shattered_cube_list;
