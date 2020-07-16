2e8ded79036bb337eaa3bfab17e2c231357dd3b8
codeitdown <sumitbopche01@gmail.com>
2020-07-16 19:55:53 +0530

    destroy and destroy complete  events

--- src/usecases/eventConnections.js
+++ src/usecases/eventConnections.js
@@ -1,5 +1,6 @@
 import * as EventEmitter from "events";
 import LoadMenu from "./loadMenu"
+import createShatteredCubeEntities from './createShatteredCubeEntities';
 
 export default function (){
 
@@ -14,16 +15,22 @@ export default function (){
     });
 
     window.ee.on('startWorkout', () => {
-
+      console.log('start work out event called')
     });
 
     window.ee.on('gotHit', () => {
     });
 
     
-    window.ee.on('destroy', () => {
+    window.ee.on('destroy', (entity) => {
+      entity.shattered_cube_list = createShatteredCubeEntities(entity);
+      console.log('destroy is called ')
     });
     
     window.ee.on('gameOver', () => {
+    });
+
+    window.ee.on('destroyComplete', (entity)=> {
+      entity.is_destroyed = true;
     });
 }
