157d120004dd12a381de97c1b6b58d8d7bc62d0f
codeitdown <sumitbopche01@gmail.com>
2020-07-16 08:56:08 +0530

    creates multiple small cubes based cube points loc

---
+++ src/usecases/createShatteredCubeEntities.js
@@ -0,0 +1,34 @@
+import ShatteredCube from '../entities/shatteredCubes';
+/**
+ * 
+ * @param {Object} ctx context of the canvas 
+ * @param {Object} entity current state of entity, need last points of entity to create small cubes 
+ */
+export default function createShatteredCubeEntities(entity) {
+    
+    let shattered_cube_list = [];
+    let x_min = entity.last_points[0][0], y_min = entity.last_points[0][1],
+        x_max = entity.last_points[1][2], y_max = entity.last_points[1][3];
+    //create small cubes
+    let rows = Math.floor((x_max - x_min) / 50);
+    let columns = Math.floor((y_max - y_min) / 50);
+    let current_x = x_min;
+    let current_y = y_min;
+    // console.log('rows and columns ', rows, columns);
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let data = {
+                current_x,
+                current_y,
+                cube_color: entity.cube_color,
+                gameHeight: window.gameHeight,
+                gameWidth: window.gameWidth
+            }
+            shattered_cube_list.push(new ShatteredCube(data, entity.ctx));
+            current_x += 50
+        }
+        current_y += 50;
+        current_x = x_min
+    }
+    return shattered_cube_list;
+}
