e2e7becedabee75472f4f05dec8edd05cc62f98e
codeitdown <sumitbopche01@gmail.com>
2020-07-17 02:28:56 +0530

    removed self call at random time

--- src/usecases/createEntities.js
+++ src/usecases/createEntities.js
@@ -1,18 +1,19 @@
 import Cube from '../entities/cube'
 
-export default function createEntities(entities, ctx) {
+export default function createEntities(entities, ctx, cube_color, arrow_color, arrow_direction) {
     // Empty the array of entities
     entities.length = 0;
-    function generateEntities() {
-        let time = randomInteger(1000, 2000);
-        let cube = new Cube(ctx);
+    // function generateEntities() {
+        // let time = randomInteger(4000, 6000);
+        let cube = new Cube(ctx, cube_color, arrow_color,arrow_direction);
         window.cube = cube;
         entities.push(cube);
         // setTimeout(generateEntities, time);
-    }
+    // }
 
-    generateEntities();
+    // generateEntities();
 }
+
 
 function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
